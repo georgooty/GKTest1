d800c0d9d7b784d6826b9421a3fc29a58125b6d8
georgooty <georgooty@gmail.com>
2022-06-24 20:34:15 -0400

    updated salary

--- employee.py
+++ employee.py
@@ -1,10 +1,12 @@
 class Employee:
-    def __init__(self, name, designation) -> None:
+    def __init__(self, name, designation, salary) -> None:
         self.name = name
         self.designation = designation
+        self.salary = salary
 
 
-p1 = Employee("Hari","Engineer")
+p1 = Employee("Hari","Engineer",10000)
 
 print(p1.name)
 print(p1.designation)
+print(p1.salary)
